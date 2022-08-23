7432f9c795b4bec773b6be08f00a3fa338fd65f2
Tomoaki Kawada <kawada@kmckk.co.jp>
2022-08-23 16:42:32 +0900

    doc(bcm2711_pac): add a code example to `Vpa::to_arm_pa`

--- common/bcm2711_pac/src/bus.rs
+++ common/bcm2711_pac/src/bus.rs
@@ -3,7 +3,15 @@
 pub struct Vpa(pub u64);
 
 impl Vpa {
-    /// Map this address to a low-peripheral ARM physical address.
+    /// Map a given VC address to a low-peripheral ARM physical address.
+    ///
+    /// # Example
+    ///
+    /// ```rust
+    /// use bcm2711_pac::Vpa;
+    /// let gpio = Vpa(0x4_7e20_0000);
+    /// assert_eq!(gpio.to_arm_pa(), Some(0xfe20_0000));
+    /// ```
     pub const fn to_arm_pa(self) -> Option<u64> {
         match self.0 {
             0x4_7c00_0000..=0x4_7fff_ffff => Some(self.0 - 0x4_7c00_0000 + 0xfc00_0000),
